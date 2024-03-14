f4b9781628ff150a90bbfcd3743026580662844e
Laura Semino <seminol@shernet.sheridancollege.ca>
2024-03-14 17:06:06 -0400

    Week9-Task4 Complete

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -1,19 +1,36 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CharacterControl : MonoBehaviour
 {
+    public TextMeshProUGUI PrintSelected;
+    public static CharacterControl instance;
     public static Villager SelectedVillager { get; private set; }
+
+    private void Start()
+    {
+        instance = this;
+        PrintSelected = GetComponent<TextMeshProUGUI>();
+    }
+    private void Update()
+    {
+
+        
+    }
     public static void SetSelectedVillager(Villager villager)
     {
-        if(SelectedVillager != null)
+        if (SelectedVillager != null)
         {
             SelectedVillager.Selected(false);
         }
         SelectedVillager = villager;
         SelectedVillager.Selected(true);
+        print(SelectedVillager);
+        
+        instance.PrintSelected.text = (villager.name);
     }
-    
+
 }
