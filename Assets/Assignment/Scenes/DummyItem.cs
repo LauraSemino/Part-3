8b093417c88b29ff10acc7c2dc6d1d61036b2365
Laura Semino <seminol@shernet.sheridancollege.ca>
2024-04-01 15:10:42 -0400

    Dummy Item can be moved and placed within the bounds with no issue.

---
+++ Assets/Assignment/Scenes/DummyItem.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class DummyItem : MonoBehaviour
+{
+    public float inventorySlot;
+    public float menuSlot;
+    public Cursor cursor;
+    public bool isSelected;
+    protected Vector2 pos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inventorySlot = 0;
+        menuSlot = 0;
+        isSelected = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pos = transform.position;
+        
+
+        if (cursor.selection == inventorySlot && Input.GetKeyDown(KeyCode.Z) && cursor.menu == menuSlot)
+        {
+            isSelected = true;
+        }
+        if (isSelected == true )
+        {
+            pos.x = cursor.transform.position.x + 2;
+            pos.y = cursor.transform.position.y;
+        }
+        if(isSelected == true && Input.GetKeyDown(KeyCode.Z) && (cursor.selection != inventorySlot || cursor.menu != menuSlot))
+        {
+           isSelected = false;
+           inventorySlot = cursor.selection;
+           menuSlot = cursor.menu;
+        }
+        if (isSelected == true && Input.GetKeyDown(KeyCode.X))
+        {
+            isSelected = false;
+            lastItemPosition();
+        }
+
+        transform.position = pos;
+    }
+    void lastItemPosition()
+    {
+        pos.y = -inventorySlot + 3.75f;
+        if (menuSlot == 0)
+        {
+            pos.x = -6.88f;
+        }
+        if(menuSlot == 1)
+        {
+            pos.x = 0.65f;
+        }
+        
+    }
+}
